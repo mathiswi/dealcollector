683b3fa30c4a2e9193cebc14871fc13baf4a6278
Mathis Witte <mail@mwitte.dev>
2022-01-19 02:10:42 +0100

    Fix image fetching scraping aldi page

--- packages/aldi/getImageUrl.ts
+++ packages/aldi/getImageUrl.ts
@@ -1,8 +1,8 @@
 export function getImageUrl(img: HTMLImageElement): string {
-  const srcsets: Array<string> = img?.dataset?.srcset?.split(',') || [];
+  const srcsets: Array<string> = img?.srcset?.split(',') || [];
   if (srcsets.length < 1)
     return '';
   const tempString: string = srcsets.slice(-1)[0];
   const imgUrl: string = tempString.split(' ')[1];
-  return (`https://www.aldi-nord.de/${imgUrl}`);
+  return (`https://www.aldi-nord.de${imgUrl}`);
 }
