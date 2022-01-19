d289c39cb623951548fa2d2fab4844c80cc27708
Mathis Witte <mail@mwitte.dev>
2022-01-19 02:17:24 +0100

    Scrape lower quality image

--- packages/aldi/getImageUrl.ts
+++ packages/aldi/getImageUrl.ts
@@ -2,7 +2,7 @@ export function getImageUrl(img: HTMLImageElement): string {
   const srcsets: Array<string> = img?.srcset?.split(',') || [];
   if (srcsets.length < 1)
     return '';
-  const tempString: string = srcsets.slice(-1)[0];
+  const tempString: string = srcsets.slice(-4)[0];
   const imgUrl: string = tempString.split(' ')[1];
   return (`https://www.aldi-nord.de${imgUrl}`);
 }
